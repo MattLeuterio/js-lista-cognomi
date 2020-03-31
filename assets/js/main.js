b34c38631bd26ae8842dc5e1f90bddaab24c3b78
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-03-31 19:07:51 +0200

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,51 +5,35 @@
     - scrivi anche la posizione della lista in cui il nuovo utente si trova
  */
 
- // References
-
- var aList = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
- var surnameInput = document.getElementById('surnameInput');
- var listCont = document.getElementById('list-container');
- var btnAdd = document.getElementById('btnAdd')
-
- var btnAdd = document.getElementById('btnAdd');
- btnAdd.addEventListener('click',
-    function() {
-        aList.push(surnameInput.value);
-        newAdd = '<li>' + surnameInput.value + '</li>';
-        listCont.innerHTML += newAdd;
-        aList.sort();        
-        console.log(aList);   
-    }
- );
-
- for (var i = 0; i < aList.length; i++) {      
-    aList.sort(); 
-    var surname = aList[i];
-    var allSurname = listCont.innerHTML;
-    var newSurname = '<li>' + surname + '</li>';
-    var all = allSurname + newSurname;
-    listCont.innerHTML = all;
- }   
-
  
-
- //Declare variables
- var usersArray = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
- var usersArraySorted;
- var usersInput;
- var usersInputInArray;
+var list = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var surnameInput = document.getElementById('surnameInput');
+var listCont = document.getElementById('list-container');
+var btnAdd = document.getElementById('btnAdd');
 
 
-//Force a non-empty input
- do {
-   usersInput = prompt('Your surname, sir?');
- } while (usersInput == '');
 
-//Add the input to the array and sort it, giving a before/after output
-usersArray.push(usersInput);
-usersInputInArray =  usersArray.indexOf(usersInput);
-console.log('Original position is ', usersInputInArray, 'in the array ', usersArray);
-usersArraySorted = usersArray.sort();
-console.log('Sorted position is ', usersArraySorted.indexOf(usersInput), 'in the array ', usersArraySorted);
- 
+console.log(list);
+
+btnAdd.addEventListener('click',
+   function() {
+      var items = '';
+      list.push(surnameInput.value);   
+      
+
+      for (var i = 0; i < list.length; i++) {
+      list.sort();   
+      items += '<li>' + list[i] + '</li>';
+      }
+
+      listCont.innerHTML = items;
+
+      var posItem = list.indexOf(surnameInput.value) + 1;
+      console.log(posItem);
+      document.getElementById('position').innerHTML = posItem;
+      document.getElementById('pos-visibility').classList.remove('u_hidden'); 
+   
+      surnameInput.value = '';
+      surnameInput.focus(); 
+   }
+);
